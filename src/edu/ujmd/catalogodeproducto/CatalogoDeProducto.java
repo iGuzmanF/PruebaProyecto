6d8c17554e6cb408c466d16a7f57f782f5043fee
iGuzmanF <201900215@ujmd.edu.sv>
2021-08-22 15:26:49 -0600

    Catalogo de producto

---
+++ src/edu/ujmd/catalogodeproducto/CatalogoDeProducto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ujmd.catalogodeproducto;
+
+/**
+ *
+ * @author chele
+ */
+public class CatalogoDeProducto {
+
+ public static void main(String[] args) {
+     
+ System.out.println("Catalogo de productos:");
+ System.out.print("1- huevos\n" +
+                "2- pollo\n" +
+                "3- aceite\n" +
+                "4- fósforos\n" +
+                "5- dulces\n" +
+                "6- margarina\n" +
+                "7- jabón\n" +
+                "8- carne\n" +
+                "9- gaseosa\n" +
+                "10- desechables\n");
+    }
+}
