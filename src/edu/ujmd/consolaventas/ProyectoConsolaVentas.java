c3ae650558136428d66e0462bb383548cb191b6c
MarioE99 <meev99@hotmail.com>
2021-08-22 15:11:20 -0600

    Proyecto principal

---
+++ src/edu/ujmd/consolaventas/ProyectoConsolaVentas.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ujmd.consolaventas;
+
+import edu.ujmd.Clases.Usuarios;
+import java.util.Scanner;
+
+/**
+ *
+ * @author meev9
+ */
+public class ProyectoConsolaVentas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Declaramos variables
+        
+        String usuario;
+        
+        String cont;
+        
+        //Realizamos el Login para los usuarios
+        
+       System.out.println("Bienvenidos al sistema de ventas Don Diego");
+       
+       System.out.println("Ingrese las credenciales");
+        
+       Scanner in = new Scanner(System.in);
+       
+      
+       
+      
+         //Realizamos el menu de acceso    
+        System.out.println("Ingrese el usuario ");
+        
+        usuario = in.nextLine();
+        
+        System.out.println("Ingrese la contraseña");
+        
+        cont = in.nextLine();
+        
+        
+            
+        
+       
+        
+    }
+    
+}
